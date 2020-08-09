c0774ad709534d301b2314018ad4d358a50f823f
Your Name <you@example.com>
2020-08-09 14:41:25 +0530

    create generateMessage utility

---
+++ server/utils/message.test.js
@@ -0,0 +1,12 @@
+var expect = require('expect');
+var {generateMessage}  = require('./message');
+
+describe('generateMessage', ()=>{
+    it('should generate correct message object', ()=>{
+        var from = 'Annna';
+        var text = 'some text';
+        var message = generateMessage(from, text);
+        expect(typeof message.createdAt).toBe('number');
+        expect(message).toMatchObject({from, text});
+    });
+});
